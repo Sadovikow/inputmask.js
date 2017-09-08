4f7ccc3422db30b8705c11700e8026fb6e371040
SerezhkaNEXX <nexxerr@gmail.com>
2017-09-08 11:11:15 +0300

    v 1

---
+++ script.js
@@ -0,0 +1,5 @@
+  $("input#phone").inputmask("7(999)999-99-99",{
+      oncomplete: function(){ },
+      onincomplete: function(){ $(this).val(""); },
+      oncleared: function(){ $(this).val(""); },
+    });
